32a0df7aa8404966741c401d3c5089d47559b958
SARUXSARU <lukasz.muras25@gmail.com>
2024-04-19 13:09:13 +0200

    add properties

--- src/main/java/com/example/agroMarket/AgroMarketApplication.java
+++ src/main/java/com/example/agroMarket/AgroMarketApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AgroMarketApplication {
-
+//com
 	public static void main(String[] args) {
 		SpringApplication.run(AgroMarketApplication.class, args);
 	}
