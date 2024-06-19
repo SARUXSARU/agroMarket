7c17b898dd4c6aa0af9abc66d33b595c1677fad6
SARUXSARU <lukasz.muras25@gmail.com>
2024-06-19 10:09:12 +0200

    @SARUXASARU - dir change

--- README.md
+++
@@ -1,1 +0,0 @@
-App for a bachelor's thesis
--- src/main/java/com/example/agroMarket/ad/AdRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.agroMarket.ad;
-
-import com.example.agroMarket.ad.entity.AdEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AdRepository extends MongoRepository<AdEntity,String> {
-
-}
